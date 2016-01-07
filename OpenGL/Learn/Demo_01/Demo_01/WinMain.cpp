c2c411b0a0520ad8ac1fbdcb9bc6b26e7ceecf7f
RussellWait <yiliusan163@163.com>
2016-01-08 01:25:40 +0800

    2016.01.08-1:25

--- OpenGL/Learn/Demo_01/Demo_01/WinMain.cpp
+++ OpenGL/Learn/Demo_01/Demo_01/WinMain.cpp
@@ -22,16 +22,18 @@ void setupPixelFormat(HDC hdc)
 		sizeof(PIXELFORMATDESCRIPTOR),		// 描述结构的大小
 		1,									// 版本号
 
-		PFD_DRAW_TO_WINDOW	|				// 描述像素缓冲区属性标志：		缓冲区可以绘制到一个窗口或者设备表面
-		PFD_SUPPORT_OPENGL	|				//								缓冲区支持OpenGL绘制
-		PFD_DOUBLEBUFFER,					//								支持双缓冲，与PFD_SUPPORT_GDI互斥
-											//	PFD_DEPTH_DONTCARE			被请求的像素格式，可以带也可以不带深度缓冲区，不指定的话仅带深度缓冲区的像素格式被考虑
-											//	PFD_DOUBLEBUFFER_DONTCARE	像素格式可以是单一的也可以是双缓冲区的
-											//	PFD_GENERIC_ACCELERATED		像素格式呗设备驱动器所加速
-											//	PFD_GENERIC_FORMAT			像素格式仅在软件中被支持
+											// 描述像素缓冲区属性标志：
+		PFD_DRAW_TO_WINDOW	|				//		PFD_DRAW_TO_WINDOW			缓冲区可以绘制到一个窗口或者设备表面
+		PFD_SUPPORT_OPENGL	|				//		PFD_SUPPORT_OPENGL			缓冲区支持OpenGL绘制
+		PFD_DOUBLEBUFFER,					//		PFD_DOUBLEBUFFER			支持双缓冲，与PFD_SUPPORT_GDI互斥
+											//		PFD_DEPTH_DONTCARE			被请求的像素格式，可以带也可以不带深度缓冲区，不指定的话仅带深度缓冲区的像素格式被考虑
+											//		PFD_DOUBLEBUFFER_DONTCARE	像素格式可以是单一的也可以是双缓冲区的
+											//		PFD_GENERIC_ACCELERATED		像素格式呗设备驱动器所加速
+											//		PFD_GENERIC_FORMAT			像素格式仅在软件中被支持
 
-		PFD_TYPE_RGBA,						// 描述像素数据类型：			红、绿、蓝、透明
-											//	PFD_TYPE_COLORINDEX			调色板模式
+											// 描述像素数据类型：
+		PFD_TYPE_RGBA,						//		PFD_TYPE_RGBA				红、绿、蓝、透明
+											//		PFD_TYPE_COLORINDEX			调色板模式
 
 		32,									// 每个像素的字节位数，就是我们看到图片颜色的位数
 		0, 0, 0, 0, 0, 0, 0, 0,				// 红色字节位数、以为计数，绿色...，蓝色...，alpha通道...
@@ -39,9 +41,10 @@ void setupPixelFormat(HDC hdc)
 		0, 0, 0, 0,							// 红、绿、蓝、alpha累加字节位数
 		32, 0, 0,							// 深度、特征、辅助缓冲区字节位数
 
-		PFD_MAIN_PLANE,						// 绘图图层类型：				设为主图层
-											//	PFD_OVERLAY_PLANE			
-											//	PFD_UNDERLAY_PLANE			
+											// 绘图图层类型：
+		PFD_MAIN_PLANE,						//		PFD_MAIN_PLANE				设为主图层
+											//		PFD_OVERLAY_PLANE			设为上层
+											//		PFD_UNDERLAY_PLANE			设为底层
 
 		0,									// 所支持的上层或下层平面的数量
 		0, 0, 0								// 不再使用
@@ -65,8 +68,21 @@ LRESULT CALLBACK WindowProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
 			setupPixelFormat(hdc);
 
 			// 创建渲染的环境
+			// wglCreateContext 创建一个新的OpenGL渲染描述表
+			// 此描述表必须适用于绘制到hdc返回的设备
+			// 此描述表将有和设备上下文（dc）一样的像素格式
+			// 如果编译报错加载 glaux.lib glu32.lib OpenGL32.lib
 			hrc = wglCreateContext(hdc);
 
+
+			// 设定当前设备的环境以及渲染环境
+			// wglMakeCurrent 设定OpenGL当前线程的渲染环境
+			// 以后这个线程所有的OpenGL调用都是在这个hdc标识的设备上绘制
+			// 你也可以使用 wglMakeCurrent 来改变调用线程的当前渲染环境，使之不再是当前的渲染环境
+			
+			// hdc：设备环境的句柄，调用这个函数的线程接下来的所有OpenGL调用倒在这个hdc所标识的设备上绘制
+			// hrc：函数设定的OpenGL渲染环境的句柄，作为当前线程的渲染环境
+			//		如果hrc为NULL，函数将使调用线程的当前渲染环境不再作为当前的渲染环境，并释放这个渲染环境所使用的设备环境。此时hdc参数将被忽略
 			wglMakeCurrent(hdc, hrc);
 
 			return (0);
@@ -108,7 +124,8 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevinstance, LPSTR lpCmdLine
 		return (0);
 	}
 
-	if (!(hWnd = CreateWindowEx(NULL, "Test", NULL, WS_OVERLAPPED, 0, 0, 640, 480, NULL, NULL, hinstance, NULL)))
+	if (!(hWnd = CreateWindowEx(NULL, "Test", NULL, WS_OVERLAPPEDWINDOW, 0, 0, 640, 480,
+								NULL, NULL, hinstance, NULL)))
 	{
 		return (0);
 	}
@@ -118,6 +135,7 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevinstance, LPSTR lpCmdLine
 
 	while (TRUE)
 	{
+		// 检测消息
 		if (PeekMessage(&msg, NULL, 0, 0, PM_REMOVE))
 		{
 			if (WM_QUIT == msg.message)
@@ -125,6 +143,7 @@ int WINAPI WinMain(HINSTANCE hinstance, HINSTANCE hPrevinstance, LPSTR lpCmdLine
 				break;
 			}
 
+			// 消息转换
 			TranslateMessage(&msg);
 			DispatchMessage(&msg);
 		}
