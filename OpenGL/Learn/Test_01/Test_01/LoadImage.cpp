c189cc6e43e2770ece78fa2213708ba9d4a882c2
RussellWait <yiliusan163@163.com>
2016-02-16 00:46:45 +0800

    2016.02.16-00:46

---
+++ OpenGL/Learn/Test_01/Test_01/LoadImage.cpp
@@ -0,0 +1,35 @@
+#include "LoadImage.h"
+
+
+BOOL CLoadImage::LoadBMP(char *szFileName)
+{
+    BOOL state = FALSE;
+
+    AUX_RGBImageRec *textureImg[1];
+    if ( textureImg[0] = auxDIBImageLoad(szFileName) )
+    {
+        state = TRUE;
+
+        m_szFileName    = szFileName;
+        m_uiWidth       = textureImg[0]->sizeX;
+        m_uiHeight      = textureImg[0]->sizeY;
+        m_uiFileLen     = sizeof(textureImg[0]->data);
+
+        glGenTextures(1, m_texture);
+        glBindTexture(GL_TEXTURE_2D, m_texture[0]);
+        glTexImage2D(GL_TEXTURE_2D, 0, 3, m_uiWidth, m_uiHeight, 0, GL_RGB, GL_UNSIGNED_BYTE, textureImg[0]->data);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+    }
+
+    if (textureImg[0])
+    {
+        if (textureImg[0]->data)
+        {
+            free(textureImg[0]->data);
+        }
+        free(textureImg[0]);
+    }
+
+    return state;
+}
--- OpenGL/Learn/Test_01/Test_01/WinMain.cpp
+++ OpenGL/Learn/Test_01/Test_01/WinMain.cpp
@@ -5,6 +5,7 @@
 #include <gl/GL.H>
 #include <gl/GLU.H>
 #include <gl/GLAUX.H>
+#include "3D_Function.h"
 
 
 #define WND_CLASS_NAME		"Test"
@@ -23,117 +24,135 @@ HDC		main_hdc;
 
 int WINAPI WinMain(_In_ HINSTANCE hInstance, _In_opt_ HINSTANCE hPrevInstance, _In_ LPSTR lpCmdLine, _In_ int nShowCmd)
 {
-	MSG msg;
+    MSG msg;
 
-	if ( !MyRegisterClass(hInstance) )
-	{
-		return FALSE;
-	}
+    if ( !MyRegisterClass(hInstance) )
+    {
+        return FALSE;
+    }
 
-	if ( !InitInstance(hInstance, nShowCmd) )
-	{
-		return FALSE;
-	}
+    if ( !InitInstance(hInstance, nShowCmd) )
+    {
+        return FALSE;
+    }
 
-	while ( TRUE )
-	{
-		if ( PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) )
-		{
-			if ( WM_QUIT == msg.message )
-			{
-				break;
-			}
+    while ( TRUE )
+    {
+        if ( PeekMessage(&msg, NULL, 0, 0, PM_REMOVE) )
+        {
+            if ( WM_QUIT == msg.message )
+            {
+                break;
+            }
 
-			TranslateMessage(&msg);
-			DispatchMessage(&msg);
-		}
-	}
+            TranslateMessage(&msg);
+            DispatchMessage(&msg);
+        }
+    }
 
-	return msg.wParam;
+    return msg.wParam;
 }
 
 ATOM MyRegisterClass(HINSTANCE hInstance)
 {
-	WNDCLASSEX wndClass;
+    WNDCLASSEX wndClass;
 
-	wndClass.cbSize			= sizeof(WNDCLASSEX);
-	wndClass.style			= CS_DBLCLKS | CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
-	wndClass.lpfnWndProc	= WndProc;
-	wndClass.cbClsExtra		= 0;
-	wndClass.cbWndExtra		= 0;
-	wndClass.hInstance		= hInstance;
-	wndClass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
-	wndClass.hCursor		= LoadCursor(NULL, IDC_ARROW);
-	wndClass.hbrBackground	= (HBRUSH)GetStockObject(BLACK_BRUSH);
-	wndClass.lpszMenuName	= NULL;
-	wndClass.lpszClassName	= WND_CLASS_NAME;
-	wndClass.hIconSm		= LoadIcon(NULL, IDI_APPLICATION);
+    wndClass.cbSize			= sizeof(WNDCLASSEX);
+    wndClass.style			= CS_DBLCLKS | CS_OWNDC | CS_HREDRAW | CS_VREDRAW;
+    wndClass.lpfnWndProc	= WndProc;
+    wndClass.cbClsExtra		= 0;
+    wndClass.cbWndExtra		= 0;
+    wndClass.hInstance		= hInstance;
+    wndClass.hIcon			= LoadIcon(NULL, IDI_APPLICATION);
+    wndClass.hCursor		= LoadCursor(NULL, IDC_ARROW);
+    wndClass.hbrBackground	= (HBRUSH)GetStockObject(BLACK_BRUSH);
+    wndClass.lpszMenuName	= NULL;
+    wndClass.lpszClassName	= WND_CLASS_NAME;
+    wndClass.hIconSm		= LoadIcon(NULL, IDI_APPLICATION);
 
-	return RegisterClassEx(&wndClass);
+    return RegisterClassEx(&wndClass);
 }
 
 BOOL InitInstance(HINSTANCE hInstance, int nShowCmd)
 {
-	HWND hWnd = CreateWindowEx(NULL,
-							   WND_CLASS_NAME,
-							   NULL,
-							   WS_OVERLAPPEDWINDOW,
-							   0, 0, WND_WIDTH, WND_HEIGHT,
-							   NULL,
-							   NULL,
-							   hInstance,
-							   NULL);
+    HWND hWnd = CreateWindowEx(NULL,
+                               WND_CLASS_NAME,
+                               NULL,
+                               WS_OVERLAPPEDWINDOW,
+                               0, 0, WND_WIDTH, WND_HEIGHT,
+                               NULL,
+                               NULL,
+                               hInstance,
+                               NULL);
 
-	if ( !hWnd )
-	{
-		return FALSE;
-	}
+    if ( !hWnd )
+    {
+        return FALSE;
+    }
 
-	ShowWindow(hWnd, nShowCmd);
-	UpdateWindow(hWnd);
+    ShowWindow(hWnd, nShowCmd);
+    UpdateWindow(hWnd);
 
-	return TRUE;
+    return TRUE;
 }
 
 LRESULT CALLBACK WndProc(HWND hWnd, UINT message, WPARAM wParam, LPARAM lParam)
 {
-	switch ( message )
-	{
-		case WM_CREATE:
-		{
-						  main_hdc = GetDC(hWnd);
-						  SetupPixcelFormat(main_hdc);
+    switch ( message )
+    {
+        case WM_CREATE:
+        {
+            main_hdc = GetDC(hWnd);
+            SetupPixcelFormat(main_hdc);
 
-						  HGLRC hrc = wglCreateContext(main_hdc);
-						  wglMakeCurrent(main_hdc, hrc);
+            HGLRC hrc = wglCreateContext(main_hdc);
+            wglMakeCurrent(main_hdc, hrc);
 
-						  SetTimer(hWnd, 1, 1, NULL);
+            InitOpenGL();
+            SetupMatrices(WND_HEIGHT, WND_HEIGHT);
 
-						  return 0;
-		} break;
+            SetTimer(hWnd, 1, 1, NULL);
 
-		case WM_TIMER:
-		{
-						 return 0;
-		} break;
+            return 0;
+        } break;
 
-		case WM_DESTROY:
-		{
-						   PostQuitMessage(0);
-						   return 0;
-		} break;
+        case WM_TIMER:
+        {
+            Render();
+            SwapBuffers(main_hdc);
 
-		default:
-		break;
-	}
+            return 0;
+        } break;
 
-	return DefWindowProc(hWnd, message, wParam, lParam);
+        case WM_DESTROY:
+        {
+            PostQuitMessage(0);
+            return 0;
+        } break;
+
+        default:
+            break;
+    }
+
+    return DefWindowProc(hWnd, message, wParam, lParam);
 }
 
 void SetupPixcelFormat(HDC hdc)
 {
-	//PIXELFORMATDESCRIPTOR pfd =
-	//{
+    PIXELFORMATDESCRIPTOR pfd =
+    {
+        sizeof(PIXELFORMATDESCRIPTOR),
+        1,
+        PFD_DRAW_TO_WINDOW | PFD_SUPPORT_OPENGL | PFD_DOUBLEBUFFER,
+        PFD_TYPE_RGBA,
+        32, 0, 0, 0, 0, 0, 0, 0, 0,
+        0, 0, 0, 0, 0,
+        32, 0, 0,
+        PFD_MAIN_PLANE,
+        0,
+        0, 0, 0
+    };
 
-	//};
+    int pixelFormat = ChoosePixelFormat(hdc, &pfd);
+    SetPixelFormat(hdc, pixelFormat, &pfd);
 }
